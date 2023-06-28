4f7e5bbb445194eff70c6d3f56937b61f2d8549e
kunal528 <64953335+kunal528@users.noreply.github.com>
2023-06-28 08:59:12 +0530

    api issue fixed

--- pages/api/nft.js
+++ pages/api/nft.js
@@ -4,16 +4,19 @@ var serviceAccount = require("../../lib/serviceAccount.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
-});
+},);
 
+const db = admin.firestore()
 export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     const id = req.query.id
-    const db = admin.firestore()
     const documentRef = db.collection('nfts').doc(id);
     documentRef.get().then((doc) => {
         if (doc.exists) {
             const data = doc.data()
-            const attributes = data;
+            const attributes = doc.data();
             delete attributes.name;
             delete attributes.description;
             delete attributes.image;
